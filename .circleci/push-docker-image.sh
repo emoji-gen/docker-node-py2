d951809f2413215ccae47b6cadd79780da2cf1af
Pine Mizune <pinemz@gmail.com>
2019-07-22 23:35:15 +0900

    refactor: use bash syntax

--- .circleci/push-docker-image.sh
+++ .circleci/push-docker-image.sh
@@ -2,9 +2,19 @@
 
 set -eu -o pipefail
 
-NODE_VERSION=$(fgrep 'node:' Dockerfile | sed -E 's/^.*node\:([0-9\.]+).*$/\1/')
-PYTHON_VERSION=$(fgrep 'python:' Dockerfile | sed -E 's/^.*python\:([0-9\.]+).*$/\1/')
-TAG_NAME="node$NODE_VERSION-py$PYTHON_VERSION"
+declare -r NODE_VERSION=$(
+  fgrep 'node:' Dockerfile | \
+    sed -E 's/^.*node\:([0-9\.]+).*$/\1/')
+
+declare -r PYTHON_VERSION=$(
+  fgrep 'python:' Dockerfile | \
+    sed -E 's/^.*python\:([0-9\.]+).*$/\1/')
+
+declare -r TAG_NAME="node$NODE_VERSION-py$PYTHON_VERSION"
+
+# ---------------------------------------------------------
+
+set -x
 
 docker tag $DOCKER_OWNER/$DOCKER_IMAGE:latest $DOCKER_OWNER/$DOCKER_IMAGE:$TAG_NAME
 docker push $DOCKER_OWNER/$DOCKER_IMAGE
