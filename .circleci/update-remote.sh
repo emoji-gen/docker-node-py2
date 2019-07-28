3db563383f863a8d11795463292c9873fefe90fa
Pine Mizune <pinemz@gmail.com>
2019-07-28 18:21:12 +0900

    fix: add `update-remote.sh`

---
+++ .circleci/update-remote.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -n "$(git --no-pager diff --raw)" ]; then
+  echo 'The worktree is dirty.'
+
+  git config user.name 'Emoji Generator'
+  git config user.email 'ultimate.emoji.gen@gmail.com'
+
+  export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no'
+  git stash -u
+  git checkout master
+  git pull origin master
+  git stash pop
+  git status
+  git commit -a -m 'chore(Dockerfile): update images'
+  git status
+  git push origin master
+fi
